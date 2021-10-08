f7917dc9cf341b090540d864e71496237194cca2
Tudo12 <gfirman923@gmail.com>
2021-10-08 19:38:47 +0700

    Last update

--- plugins/main.py
+++ plugins/main.py
@@ -1,15 +1,18 @@
 from lightbulb import Plugin, command, owner_only
+import lightbulb
 from time import time
 import lightbulb
 import random
 import hikari
+import sys
+sys.path.append('Modules/')
+import utils
 
 class Core(Plugin):
 
     @command()
     async def ping(self, ctx):
-        """Check if I am alive at this present moment"""
-
+        """Ping pong"""
         start = time()
         msg = await ctx.respond(
             content = "Pong",
@@ -22,6 +25,14 @@ class Core(Plugin):
                 color = random.randint(0, 0xffffff)
             )
         )
+
+    @lightbulb.check(lightbulb.owner_only)
+    @command(aliases = ['logout'])
+    async def shutdown(self, ctx):
+        """Shut down the bot"""
+        await ctx.respond(embed = Utils.loading_embed())
+        await ctx.message.add_reaction("👋")
+        return await ctx.bot.close()
 
 def load(bot):
     bot.add_plugin(Core())
